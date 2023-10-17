37d177c28b303baf1ed6f883f0ccadad6dd2487f
subzero <shubham8111994@gmail.com>
2023-10-17 11:18:11 +0530

    Added Temperature and currency converter

---
+++ app/src/main/java/com/example/splashscreen/AddQuantityActivity.kt
@@ -0,0 +1,11 @@
+package com.example.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddQuantityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_quantity)
+    }
+}
